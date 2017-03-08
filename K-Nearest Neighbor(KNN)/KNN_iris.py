9ef1c6ea05350758d1039f6c3eb06b1670544a54
Leo Zhong <zhong5930@gmail.com>
2017-03-08 00:01:17 -0500

    KNN

---
+++ K-Nearest Neighbor(KNN)/KNN_iris.py
@@ -0,0 +1,19 @@
+'''
+Created on Mar 7, 2017
+
+@author: Leo Zhong
+'''
+from sklearn import neighbors
+from sklearn import datasets
+
+knn = neighbors.KNeighborsClassifier()
+
+iris = datasets.load_iris()
+
+print (iris)
+
+knn.fit(iris.data, iris.target)
+
+predictedLabel = knn.predict([[0.1, 0.2, 0.3, 0.4]])
+
+print (predictedLabel)
