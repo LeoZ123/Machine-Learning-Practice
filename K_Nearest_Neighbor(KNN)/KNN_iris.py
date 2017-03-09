b3d9bd605ada61b94d3ba262537fd9c889e33c5b
Leo Zhong <zhong5930@gmail.com>
2017-03-09 14:38:49 -0500

    Update KNN_iris.py

--- K_Nearest_Neighbor(KNN)/KNN_iris.py
+++ K_Nearest_Neighbor(KNN)/KNN_iris.py
@@ -6,14 +6,18 @@ Created on Mar 7, 2017
 from sklearn import neighbors
 from sklearn import datasets
 
+#Get KNN Classifier
 knn = neighbors.KNeighborsClassifier()
 
+#Get iris data set
 iris = datasets.load_iris()
 
 print (iris)
 
+#Training Data
 knn.fit(iris.data, iris.target)
 
+#Perdict data 
 predictedLabel = knn.predict([[0.1, 0.2, 0.3, 0.4]])
 
 print (predictedLabel)
